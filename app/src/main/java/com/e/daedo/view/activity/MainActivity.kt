cc58d892d895cc65673eac9a497f3d68271ebe3f
Jominseo <minseocho0309@gmail.com>
2021-01-07 17:10:52 +0900

    [update] 시작화면 xml디자인민 colors수정

--- app/src/main/java/com/e/daedo/view/activity/MainActivity.kt
+++ app/src/main/java/com/e/daedo/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.e.daedo
+package com.e.daedo.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.e.daedo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/e/daedo/view/fragment/IntroFragment.kt
+++ app/src/main/java/com/e/daedo/view/fragment/IntroFragment.kt
@@ -1,10 +1,11 @@
-package com.e.daedo
+package com.e.daedo.view.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.e.daedo.R
 
 class IntroFragment : Fragment() {
     override fun onCreateView(
