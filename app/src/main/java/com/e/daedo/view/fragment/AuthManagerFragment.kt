23c922291cb970e7ea6ad6ed482b487f28ada1f1
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2021-01-09 03:26:18 +0900

    Update AuthManagerFragment.kt

--- app/src/main/java/com/e/daedo/view/fragment/AuthManagerFragment.kt
+++ app/src/main/java/com/e/daedo/view/fragment/AuthManagerFragment.kt
@@ -9,20 +9,21 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.e.daedo.R
 import com.e.daedo.base.BaseFragment
+import com.e.daedo.databinding.FragmentSignUpBinding
+import com.e.daedo.viewModel.AuthManagerViewModel
+import com.e.daedo.viewModel.SignUpViewModel
 import kotlinx.android.synthetic.main.fragment_auth_manager.*
 import kotlinx.android.synthetic.main.fragment_auth_manager.view.*
 
-class AuthManagerFragment : BaseFragment {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_auth_manager, container, false)
-        view.email_btn.setOnClickListener {
-            Log.d("애미","애미")
-            findNavController().navigate(R.id.action_authManageFragment_to_loginFragment)
-        }
-        return view
+class AuthManagerFragment : BaseFragment<FragmentSignUpBinding, AuthManagerViewModel>() {
+    override val viewModel: AuthManagerViewModel = AuthManagerViewModel()
+    override val layoutRes: Int = R.layout.fragment_auth_manager
+    override fun init() {
+        TODO("Not yet implemented")
     }
+
+    override fun observerViewModel() {
+        TODO("Not yet implemented")
+    }
+
 }
