9d2677cbd95d793727c56cc89b2b31c03d89d6ff
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2021-01-09 04:51:08 +0900

    [Add]Navigation

--- app/src/main/java/com/e/daedo/view/fragment/AuthManagerFragment.kt
+++ app/src/main/java/com/e/daedo/view/fragment/AuthManagerFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.Navigator
@@ -21,17 +22,19 @@ import com.e.daedo.viewModel.AuthManagerViewModel
 import com.e.daedo.viewModel.SignUpViewModel
 import kotlinx.android.synthetic.main.fragment_auth_manager.*
 import kotlinx.android.synthetic.main.fragment_auth_manager.view.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class AuthManagerFragment : BaseFragment<FragmentAuthManagerBinding, AuthManagerViewModel>() {
     override val viewModel: AuthManagerViewModel = AuthManagerViewModel()
     override val layoutRes: Int = R.layout.fragment_auth_manager
+
     override fun init() {
 
     }
 
     override fun observerViewModel() {
-            email_btn.setOnClickListener {
-                Navigation.findNavController(requireActivity(),R.id.action_authManageFragment_to_loginFragment)
+           email_btn.setOnClickListener {
+                findNavController().navigate(R.id.action_authManagerFragment_to_loginFragment2)
             }
         }
     }
