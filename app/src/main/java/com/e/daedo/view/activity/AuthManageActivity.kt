5a5dd2436094915f576b0adaf3aa06e6765e06f5
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2021-01-08 23:21:30 +0900

    [Add]AuthManageFragment
    fragment추가,activty이름 변경

--- app/src/main/java/com/e/daedo/view/activity/AuthManageActivity.kt
+++ app/src/main/java/com/e/daedo/view/activity/AuthManageActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.e.daedo.R
 
-class MainActivity : AppCompatActivity() {
+class AuthManageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth_manage)
     }
 }
