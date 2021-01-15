70f064689fbbd7c6477d65d466d5bf9e93626f46
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-01-15 23:37:30 +0900

    [update] di with koin

--- app/src/main/java/com/e/daedo/network/RetrofitClient.kt
+++ app/src/main/java/com/e/daedo/network/RetrofitClient.kt
@@ -1,17 +1,17 @@
 package com.e.daedo.network
 
+import com.e.daedo.di.applications.MyApplication
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private var instance: Retrofit?= null
     private var API: Service?= null
-    private val baseUrl: String= "https://coatcode.herokuapp.com"
 
     fun getInstance(): Retrofit {
         if (instance == null) {
             instance = Retrofit.Builder()
-                .baseUrl(baseUrl)
+                .baseUrl(MyApplication.baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
